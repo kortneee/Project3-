e5cd76cd6e1c047573f5c925d94f5c3b4fc3c77d
kortneee <71471976+kortneee@users.noreply.github.com>
2020-11-04 23:27:21 -0600

    Add files via upload

--- varsub.py
+++ varsub.py
@@ -1,4 +1,4 @@
-""""Replaces references to variables associated with them"""
+""""Module which replaces references to variables associated them, from a dictionary"""
 # MSC 260 Fall 2020 Project 3
 # Kortnee Reiss
 # Decleration: I, Kortnee Reiss, am the sole author of this code, which was developed in 
@@ -7,8 +7,11 @@ import os
 def substitute(vars,s):
     """Takes dictionary vars, and replaces the string, s, with the variables's matching value
     in vars"""
-    varin= open(vars,"r")
-    dictvals = dict(x.split("=") for x in varin.split())
-    for key, value in dictvals.items():
-        if key == s: 
-            return value
+    words = s.split()
+    for w in words:
+        if w[0] == "$" and w[:-1] == "$":
+            for key, value in vars.items():
+                if key == w:
+                    print (value) 
+                if key == w[1:-1]:
+                    print(value)
