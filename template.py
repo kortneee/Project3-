ae7ce3bf3787e872a1dc349a3f38c9f43a4f5492
kortneee <71471976+kortneee@users.noreply.github.com>
2020-11-06 10:38:16 -0600

    Add files via upload

--- template.py
+++ template.py
@@ -7,19 +7,29 @@
 import sys
 import os
 import varsub
-if len(sys.argv) != 3: 
-    print("Usage: {} INFILE OUTFILE".format(sys.argv[0]))
-    print("The text file INFILE is then converted to an HTML file OUTFILE.")
+# take input and output files 
+try: 
+    varsfn = sys.argv[1]    #variable filename
+    infn = sys.argv[2]      #input text filename
+    outfn = sys.argv[3]     #output HTML filename
+except IndexError: 
+    print("template.py requires three command line arguments")
+    print("Usage: python template.py variable.txt infile.txt outfile.html")
     sys.exit()
 
-# take input and output files
-varsfn = sys.argv[1]    #variable filename
-infn = sys.argv[2]      #input text filename
-outfn = sys.argv[3]     #output HTML filename
-
 # open input and output files
-fin = open(infn,"r")
-fout = open(outfn,"w")
+try:
+    fin = open(infn,"r")
+except OSError: 
+    ("Input file does not exist")
+try: 
+    varin = open(varsfn, "r")
+except OSError: 
+    ("Variable file does not exist")
+try:
+    fout = open(outfn,"w")
+except OSError: 
+    ("Output file cannot be opened")
 
 # print header
 fout.write("<!DOCTYPE html>\n")
@@ -27,28 +37,25 @@ fout.write("<html>\n")
 fout.write("<head><title>HTML document</title></head>\n")
 fout.write("<body>\n")
 opentags = []  # Stack containing open tags inside <body>
-               # (only a single p tag, in this program)
 for line in fin:
     isblank = not bool(line.strip())
     if opentags and isblank:
         fout.write("</{}>\n".format(opentags.pop()))
     if not isblank:
-        # Line is not blank, must appear in output
+        # Line is not blank must appear in output
+        ls = line.strip()
         if not opentags:
             # No tags open; start a new paragraph
             fout.write("<p>\n")
             opentags.append("p")
-        line = varsub.substitute(varsfn,line)
-        words = line.split()
-        for word in words: 
-            if word[0] == '#':
-                line = ''
-                continue
-            elif word[0] == '@':
-                fout.write("<ul>\n")
-                fout.write("<li>",line)
-                fout.write("</li>\n")
-                fout.write("</ul>\n")
+        line = varsub.substitute(varsfn,ls)
+        if ls[0] == '#':
+            continue
+        elif ls[0] == '@':
+            fout.write("<ul>\n")
+            fout.write("<li>",line)
+            fout.write("</li>\n")
+            fout.write("</ul>\n")
         fout.write(line)
 # done reading from input file
 fin.close()
